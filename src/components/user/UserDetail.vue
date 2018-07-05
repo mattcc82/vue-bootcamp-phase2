07a4014179acc1c6a12de969ee3ab106381c2161
Matt Castle <mattcc82@gmail.com>
2018-07-05 13:08:53 -0400

    - router for user demonstration Signed-off-by: Matt Castle <mattcc82@gmail.com>

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -3,6 +3,11 @@
     <h2>User Detail Page - UserDetail.vue</h2>
     <hr>
     <p>id: {{ $route.params.id }}</p>
+    <b-button
+      :to="{ name: 'userEdit', params: { id: $route.params.id } }"
+      class="btn btn-info">
+      Edit User
+    </b-button>
   </section>
 </template>
 <script>
